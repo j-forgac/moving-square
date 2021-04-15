a93d6b9f4a5f50697072a8d0d91d34c21f1512c0
j-forgac <62855827+j-forgac@users.noreply.github.com>
2021-04-16 00:02:58 +0200

    j

---
+++ Triangle/src/main/java/cz/educanet/Shaders.java
@@ -0,0 +1,44 @@
+package cz.educanet;
+
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL33;
+
+public class Shaders {
+    private static final String vertexShaderSource = "#version 330 core\n" +
+            "layout (location = 0) in vec3 aPos;\n" +
+            "void main()\n" +
+            "{\n" +
+            " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n" +
+            "}";
+
+    private static final String fragmentShaderSource = "#version 330 core\n" +
+            "out vec4 FragColor;\n" +
+            "void main()\n" +
+            "{\n" +
+            "   FragColor = vec4(0.5f, 1.0f, 1.0f, 1.0f);\n" +
+            "}\n";
+
+    public static int vertexShaderId;
+    public static int fragmentShaderId;
+    public static int shaderProgramId;
+
+    public static void initShaders() {
+        vertexShaderId = GL33.glCreateShader(GL33.GL_VERTEX_SHADER);
+        fragmentShaderId = GL33.glCreateShader(GL33.GL_FRAGMENT_SHADER);
+
+        GL33.glShaderSource(vertexShaderId, vertexShaderSource);
+        GL33.glCompileShader(vertexShaderId);
+
+        GL33.glShaderSource(fragmentShaderId, fragmentShaderSource);
+        GL33.glCompileShader(fragmentShaderId);
+
+        shaderProgramId = GL33.glCreateProgram();
+        GL33.glAttachShader(shaderProgramId, vertexShaderId);
+        GL33.glAttachShader(shaderProgramId, fragmentShaderId);
+        GL33.glLinkProgram(shaderProgramId);
+
+        GL33.glDeleteShader(vertexShaderId);
+        GL33.glDeleteShader(fragmentShaderId);
+    }
+
+}
